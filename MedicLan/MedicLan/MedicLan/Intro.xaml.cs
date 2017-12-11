5816d356f852ff5ece66f001dd5f5e6df0a9df18
Njah Abdelaziz <Njah.abdelaziz@hotmail.com>
2017-12-11 23:33:23 +0100

    Intialisation of project

---
+++ MedicLan/MedicLan/MedicLan/Intro.xaml.cs
@@ -0,0 +1,51 @@
+﻿using MedicLan.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace MedicLan
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Intro : ContentPage
+    {
+        public List<CarouselData> MyDataSource { get; set; }
+        private int _position;
+        public int Position { get { return _position; } set { _position = value; OnPropertyChanged(); } }
+
+        public Intro()
+        {
+            InitializeComponent();
+
+            logoLayout.BackgroundColor = new Color(0, 0, 0, 0.2);
+
+            MyDataSource = new List<CarouselData>() { new CarouselData() { Title = "Welcome", Detail="Sign Up to see the content" },
+                                                        new CarouselData() { Title = "Explore", Detail="Explore our library" },
+                                                        new CarouselData() { Title = "Win", Detail="Win prizes" }};
+
+            BindingContext = this;
+
+        }
+
+        protected override void LayoutChildren(double x, double y, double width, double height)
+        {
+            if (width > height)
+            {
+                videoPlayer.WidthRequest = App.ScreenWidth;
+                videoPlayer.HeightRequest = App.ScreenHeight;
+            }
+            else if (width < height)
+            {
+                videoPlayer.WidthRequest = App.ScreenWidth / 2;
+                videoPlayer.HeightRequest = App.ScreenHeight / 2;
+            }
+
+            base.LayoutChildren(x, y, width, height);
+        }
+    }
+
+}
---
+++ MedicLan/MedicLan/MedicLan/Model/CarouselData.cs
@@ -0,0 +1,9 @@
+﻿using System;
+namespace MedicLan.Model
+{
+	public class CarouselData
+	{
+		public string Title { get; set; }
+		public string Detail { get; set; }
+	}
+}
